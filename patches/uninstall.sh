ac190074df75ad3c69449fb9eec6eefde078ec32
verevka <verevka86@gmail.com>
2017-03-28 13:26:57 +0300

    add patches and flags

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base frameworks/opt/telephony system/core system/netd system/sepolicy"
+dirs="bionic external/wpa_supplicant_8 frameworks/av frameworks/base frameworks/native frameworks/opt/telephony system/core system/netd system/sepolicy"
 
 for dir in $dirs ; do
 	cd $rootdirectory
