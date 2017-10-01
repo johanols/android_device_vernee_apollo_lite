3e66fea8a965edf1256506efc0fee9bb748364df
Johan Olsson <johan@jmvo.se>
2017-10-01 23:16:23 +0200

    Adding patches
    Patches for MTK support.

---
+++ patches/uninstall.sh
@@ -0,0 +1,18 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="bionic external/wpa_supplicant_8 frameworks/av frameworks/base frameworks/native frameworks/opt/telephony system/core"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Reverting $dir patches..."
+	git reset --hard
+	git clean -f -d
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
